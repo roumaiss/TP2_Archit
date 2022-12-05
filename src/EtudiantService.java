10f94876b9531b3d95334ab7a02ae11c2c4e3c9a
ChaimaKHOUDRI <118977490+ChaimaKHOUDRI@users.noreply.github.com>
2022-12-05 21:27:55 +0100

    Update EtudiantService.java

--- src/EtudiantService.java
+++ src/EtudiantService.java
@@ -3,7 +3,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-public class EtudiantService {
+public class EtudiantService implements IEtudiant  {
 	
 IJournal journal = new diffOutputJournal("def");
 	boolean inscription (int matricule, String nom, String prénom, String email,String pwd, int id_universite) throws SQLException	
