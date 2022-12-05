c2feda84dae45b9ad8ca88547e7e91f5715c0abb
ChaimaKHOUDRI <118977490+ChaimaKHOUDRI@users.noreply.github.com>
2022-12-05 13:18:18 +0100

    Update MainApp.java

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,19 +1,20 @@
 import java.sql.SQLException;
+import java.sql.*;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-
+		
 		EtudiantService serv=new EtudiantService();
+       // serv.setEtudRep(new EtudiantRepository());
+		//serv.setUnivRep(new UniversiteRepository());
+		
 		try {
-			serv.inscription(2, "Guendouziiiii", "wassila", "guen@gmail.com","xxxx", 1);
-			
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+
+		serv.inscription(2, "Guendouziiiii", "wassila", "guen@gmail.com","xxxx",1);
+	} catch (Exception e) {
+		e.printStackTrace();
+		
 	}
 
 }
