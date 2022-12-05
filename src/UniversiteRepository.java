2a7f2f4a44b67cde5de05632ff4712a857831f5e
ChaimaKHOUDRI <118977490+ChaimaKHOUDRI@users.noreply.github.com>
2022-12-05 15:31:16 +0100

    Update UniversiteRepository.java

--- src/UniversiteRepository.java
+++ src/UniversiteRepository.java
@@ -28,6 +28,23 @@ public class UniversiteRepository implements IUniversite {
 		return u;	
 	
 		
-	}	
+	}
+	public int NbrLivreAutorise(int id_univ) {
+	// TODO Auto-generated method stub
+	Universite Univ =  GetById(id_univ);
+	
+	if (Univ.getPack() == TypePackage.Standard)
+     {
+		Package pack = new Standard(null);
+        return pack.nbrLivreAutorise;
+     }
+     else if (Univ.getPack() == TypePackage.Premium)
+     {
+    	 Package pack = new Premuim(null);
+    	 return pack.nbrLivreAutorise;
+    	 }     
+	
+	return 0;
+}
 	
 }
