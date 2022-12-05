88325db892e1c3e26546f83d40f4c66ff3671f28
ChaimaKHOUDRI <118977490+ChaimaKHOUDRI@users.noreply.github.com>
2022-12-05 13:10:49 +0100

    implements IUniversite

--- src/UniversiteRepository.java
+++ src/UniversiteRepository.java
@@ -5,25 +5,26 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class UniversiteRepository {
-	
-	
-	Universite GetById(int universityId) throws SQLException {
-		
-		DBConnection BD= new DBConnection();
-		Connection connect=BD.getConn(); 
+public class UniversiteRepository implements IUniversite {
+
+
+	@Override
+	public Universite GetById(int universityId) throws SQLException {
+
+		DBConnection BD= DBConnection.getInstance();
+		Connection connect= BD.getConn();
 		Statement stmt = connect.createStatement();
-		System.out.println("LogBD : début recherche de id université dans la base de donnée");
+		System.out.println("LogBD : dÃ©but recherche de id universitÃ© dans la base de donnÃ©e");
 		
-		String sql = "select * from universite where id_universite="+ universityId;
+		String sql = "SELECT * FROM universite WHERE id_universite="+ universityId;
 		ResultSet rs = stmt.executeQuery(sql);
 		rs.next();	
 		TypePackage p=TypePackage.valueOf(rs.getString(3));
 		Universite u = new Universite (rs.getInt(1),rs.getString(2),p);
 			
-		System.out.println("LogBD : université récupérée");
+		System.out.println("LogBD : universitÃ© rÃ©cupÃ©rÃ©e");
 		
-		connect.close();
+		//connect.close();
 		return u;	
 	
 		
