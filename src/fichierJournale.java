f0186f96c344461c0491e950a8972315f20a16ba
roumaissa <119705671+roumaiss@users.noreply.github.com>
2022-12-06 12:54:32 -0800

    Update fichierJournale.java

--- src/fichierJournale.java
+++ src/fichierJournale.java
@@ -1,3 +1,9 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import Interface.IJournal;
+
 import java.io.File;
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -6,9 +12,38 @@ public class  FichierJournal implements IJournal{
   @Override
   public void outPut_Msg(String message) {
     
-    String message = System.currentTimeMillis() + Integer.toString(ThreadLocalRandom.current().nextInt(50, 1000));
-    
-    new File(message + "_journal.txt");
+    FileWriter fw = null;
+			BufferedWriter bw = null;
+			PrintWriter out = null;
+
+			try {
+			    fw = new FileWriter("log.txt", true);
+			    bw = new BufferedWriter(fw);
+			    out = new PrintWriter(bw);
+			    out.println(message);
+			    out.close();
+			}
+			catch (IOException e) {}
+
+			finally 
+			{
+			    if(out != null)
+				    out.close();
+			    try {
+			        if(bw != null)
+			            bw.close();
+			    }
+		    catch (IOException e) {}
+
+		    try 
+			    {
+			        if(fw != null)
+			            fw.close();
+			    }
+			catch (IOException e) {}
+		    
+			}
+		}
     
   }
   
