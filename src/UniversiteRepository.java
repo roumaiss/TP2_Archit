f08f33987430dc3716f42c41dbcc521d57a6dc23
roumaissa <119705671+roumaiss@users.noreply.github.com>
2022-12-05 23:06:25 -0800

    Update UniversiteRepository.java

--- src/UniversiteRepository.java
+++ src/UniversiteRepository.java
@@ -13,7 +13,7 @@ public class UniversiteRepository {
 		DBConnection BD= new DBConnection();
 		Connection connect=BD.getConn(); 
 		Statement stmt = connect.createStatement();
-		System.out.println("LogBD : début recherche de id université dans la base de donnée");
+		System.out.println("LogBD : dÃ©but recherche de id universitÃ© dans la base de donnÃ©e");
 		
 		String sql = "select * from universite where id_universite="+ universityId;
 		ResultSet rs = stmt.executeQuery(sql);
@@ -21,12 +21,28 @@ public class UniversiteRepository {
 		TypePackage p=TypePackage.valueOf(rs.getString(3));
 		Universite u = new Universite (rs.getInt(1),rs.getString(2),p);
 			
-		System.out.println("LogBD : université récupérée");
+		System.out.println("LogBD : universitÃ© rÃ©cupÃ©rÃ©e");
 		
 		connect.close();
 		return u;	
 	
 		
 	}	
+	public int NbrLivreAutorise(int id_univ) {
+	// TODO Auto-generated method stub
+	Universite Univ =  GetById(id_univ);
 	
+	if (Univ.getPack() == TypePackage.Standard)
+     {
+		Package pack = new Standard(null);
+        return pack.nbrLivreAutorise;
+     }
+     else if (Univ.getPack() == TypePackage.Premium)
+     {
+    	 Package pack = new Premuim(null);
+    	 return pack.nbrLivreAutorise;
+    	 }     
+	
+	return 0;
+}
 }
