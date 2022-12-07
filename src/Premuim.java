5f21205a7b45be2c122037d024cb416041ca878b
roumaissa <119705671+roumaiss@users.noreply.github.com>
2022-12-06 19:25:15 -0800

    Update Premuim.java

--- src/Premuim.java
+++ src/Premuim.java
@@ -1,39 +1,16 @@
 public class Premuim extends Package {
 
-	public Premuim() {
-		super(TypePackage.Premium);
+	public class Premium extends Package {
 
+	public Premium(TypePackage Tpack) {
+		super(Tpack);
 	}
-	
-	public Premuim(TypePackage nom_Tpack) {
-		super(nom_Tpack);
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void SetNbLivreAutorise(int nombre) {
-		// TODO Auto-generated method stub
-		this.nbrLivreAutorise  = nombre;
-		
-	}
-
-	@Override
-	public void SetNbLivrebonus(int nombre) {
-		// TODO Auto-generated method stub
-		this.nbrLivreBonus = nombre;
-		
-	}
-
 	@Override
 	public int getNbrLivreAutorise() {
-		// TODO Auto-generated method stub
 		return 20;
 	}
-
 	@Override
 	public int getNbrLivreBonus() {
-		// TODO Auto-generated method stub
 		return 10;
 	}
-
 }
